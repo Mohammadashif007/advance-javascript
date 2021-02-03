cc7dddb2b4d9bcac44cd88bb0887581e3e463e14
Mohammad Ashif <mohammadashif510@gmail.com>
2021-02-03 11:55:20 +0600

    true

---
+++ true-false.js
@@ -0,0 +1,36 @@
+const money = 0;
+
+if(money){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const name = "sharuk";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+
+const name1 = "";
+if(name1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+const name2 = " ";
+if(name2){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+
+
