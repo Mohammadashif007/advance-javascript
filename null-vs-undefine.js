7834d86bfeb0e4fbe8b0c9ad1491faa9f290f064
Mohammad Ashif <mohammadashif510@gmail.com>
2021-02-04 07:52:35 +0600

    null-vs-undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,44 @@
+// let jun;
+// console.log(jun);
+
+// function add(name, name1){
+//     console.log(name + name1);
+//     return
+// }
+
+
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+
+// }
+
+
+
+// let number;
+// console.log(number);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return 
+// }
+
+// const result = add(83, 12);
+// console.log(result);
+
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+
+// const pramik = {name: "smart dude", phone: 4345454};
+// console.log(pramik.gf);
+
+
+
+
+let fun = undefined;
+console.log(fun);
